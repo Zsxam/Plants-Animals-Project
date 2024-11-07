83d18dff98b3babd417b074476bdfaeaf1e1ed83
azizaputriamelia <@azizaputriamelia19@gmail.com>
2024-11-07 13:02:44 +0700

    Menambahkan code di branch main

--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-nama = input("Masukkan nama anda: ")
-tahun_lahir = int(input("Masukkan tahun lahir anda: "))
-umur = 2024 - tahun_lahir
-print(f"Selamat datang, {nama}! Umur anda adalah {umur}")
+print("Hello World")
