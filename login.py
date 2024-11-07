7bc388d7ce0a1079daa8f832b16991baca847ea7
Sina-32 <357.skbm@gmail.com>
2024-11-07 13:02:38 +0700

    Nambahin Login

--- login.py
+++ login.py
@@ -1,1 +1,15 @@
+print(f"Silahkan Login")
+Login = 3
+while Login > 0:
+    Username = input("Username : ")
+    Password = input("Password : ")
 
+    if Username == "loginUTS" and Password == "rpl2024":
+        print("Selamat datang di aplikasi prodi RPL")
+        break
+    else:
+        Login -= 1
+        if Login > 0:
+            print(f"Login Salah! Kesempatan Anda {Login}x kali lagi")
+        else:
+            print(f"Anda tidak diperkenankan mengakses aplikasi ini.")
