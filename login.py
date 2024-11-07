ee401e80f49a2e0759dc414de7e23b9e91c092c7
Sina-32 <357.skbm@gmail.com>
2024-11-07 13:04:21 +0700

    Mengubah Login

--- login.py
+++ login.py
@@ -1,15 +1,28 @@
 print(f"Silahkan Login")
-Login = 3
-while Login > 0:
-    Username = input("Username : ")
-    Password = input("Password : ")
 
-    if Username == "loginUTS" and Password == "rpl2024":
-        print("Selamat datang di aplikasi prodi RPL")
-        break
-    else:
-        Login -= 1
-        if Login > 0:
-            print(f"Login Salah! Kesempatan Anda {Login}x kali lagi")
+def login():
+    username_terdaftar = "Daspro2024"
+    password_terdaftar = "Latihan"
+    kesempatan = 5
+    
+    while kesempatan > 0:
+        username = input("Masukkan username: ")
+        password = input("Masukkan password: ")
+        
+        if username == username_terdaftar and password == password_terdaftar:
+            print("Login berhasil!")
+            return
         else:
-            print(f"Anda tidak diperkenankan mengakses aplikasi ini.")
+            kesempatan -= 1
+            if kesempatan == 0:
+                break  
+            if username != username_terdaftar and password != password_terdaftar:
+                print(f"Username dan password anda salah kesempatan tersisa: {kesempatan}")
+            elif username != username_terdaftar:
+                print(f"Username anda salah kesempatan tersisa: {kesempatan}")
+            elif password != password_terdaftar:
+                print(f"Password anda salah kesempatan tersisa: {kesempatan}")
+    
+    print("Anda telah keluar dari sistem login")
+
+login()
